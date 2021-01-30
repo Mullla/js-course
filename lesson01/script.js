e8426f2ff84e11ccb46b77b22d2e552bbd045f52
Mars <diablo_13_06@mail.ru>
2021-01-30 20:56:58 +0300

    added lesson01 folder with files

---
+++ lesson01/script.js
@@ -0,0 +1,11 @@
+let money = 100,
+    income = 250,
+    addExpenses = 4800,
+    deposit = 2000,
+    period = 5;
+
+const mission = 5000;
+
+
+alert('Hey from alert');
+console.log('Hey from console');
